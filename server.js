eecbd406dc0fd4798c9fc5dfdfaf35dbfdea9742
DongNguyen <dongnguyenvie@gmail.com>
2019-01-27 00:02:50 +0700

    config sever heroku

--- server.js
+++ server.js
@@ -13,5 +13,11 @@ app.listen(port);
 console.log('server started '+ port);
 
 
-    // "dev": "cross-env NODE_ENV=development webpack-dev-server --open --hot",
-    // "build": "cross-env NODE_ENV=production webpack --progress --hide-modules"
+// "devv": "cross-env NODE_ENV=development webpack-dev-server --open --hot",
+// "buildd": "cross-env NODE_ENV=production webpack --progress --hide-modules",
+// "dev": "webpack-dev-server --inline --progress --config build/webpack.dev.conf.js",
+// "start": "node server.js",
+// "unit": "jest --config test/unit/jest.conf.js --coverage",
+// "e2e": "node test/e2e/runner.js",
+// "test": "npm run unit && npm run e2e",
+// "build": "node build/build.js"
