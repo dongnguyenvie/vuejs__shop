c14622314c20265343fdee288fc1cdaf8821c230
DongNguyen <dongnguyenvie@gmail.com>
2019-01-26 23:40:23 +0700

    config sever

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-// app.use(serveStatic(__dirname + "index.html"));
+app.use(express.static(path.join(__dirname)))
+app.use('/dist',serveStatic(__dirname + "/dist"));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+
+    // "dev": "cross-env NODE_ENV=development webpack-dev-server --open --hot",
+    // "build": "cross-env NODE_ENV=production webpack --progress --hide-modules"
