aa1e4eaf4420afd1dda4e86d46d624ce3d0f8216
DongNguyen <dongnguyenvie@gmail.com>
2019-01-26 19:40:43 +0700

    server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const path = require('path');
-const serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-const port = process.env.PORT || 5000;
-app.listen(port)
-console.log('Server started' + port);
+// app.use(serveStatic(__dirname + "index.html"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
