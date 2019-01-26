75c543bb471fe7c6262ec1810b06e603e829cba1
DongNguyen <dongnguyenvie@gmail.com>
2019-01-26 18:46:39 +0700

    add more product

--- src/components/product-top/Product-top.vue
+++ src/components/product-top/Product-top.vue
@@ -12,11 +12,14 @@
         </div>
         <div class="product-wrap__block">
             <div class="row">
-                <div class="col-md-3 col-sm-4 col-6"
+                <div class="col-md-3 col-sm-4 col-6 block__product"
                     v-for="(product, index) in products" :key="index">
                     <div class="block__context">
                         <div class="context__img">
                             <img :src="product.images" alt="">
+                        </div>
+                        <div class="context__sell">
+                            <span>20% off</span>
                         </div>
                         <div class="context__content">
                             <p class="content__category">
@@ -24,10 +27,12 @@
                             </p>
                             <h5><a href="#">{{product.name}}</a></h5>
                             <p class="price">{{product.price}}</p>
-
+                            <button class="btn btn-secondary btn__cart">Add to cart</button>
                         </div>
                     </div>
                 </div>
+                <button class="btn block__more" href="#" 
+                    v-on:click="showMore()">Show all</button>
             </div>
             
         </div>
@@ -41,6 +46,12 @@ export default {
     data () {
         return {
             products
+        }
+    },
+    methods:{
+        showMore(){
+        //push more
+        products.length < 25 ? products.push(...products) : alert('Full');
         }
     }   
 }
