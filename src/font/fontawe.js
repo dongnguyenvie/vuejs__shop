fd9be8fadf5e62ea8649ca986809155b0c4ae3b9
DongNguyen <dongnguyenvie@gmail.com>
2019-01-27 10:31:23 +0700

    update form subscribe

--- src/font/fontawe.js
+++ src/font/fontawe.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHeart,faShoppingCart,faSearch,faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { faHeart,faShoppingCart,faSearch,faCheckSquare,faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 export default  () => {
 
-    library.add(faShoppingCart,faHeart,faSearch, faCheckSquare);
+    library.add(faShoppingCart,faHeart,faSearch, faCheckSquare, faPaperPlane);
 }
