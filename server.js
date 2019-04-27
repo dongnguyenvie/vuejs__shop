3241e7ce16626cfc356feeb39e267443ab9f5a7f
Dong Nguyen <42445585+dongnguyenvie@users.noreply.github.com>
2019-04-27 21:35:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-app = express();
-app.use(express.static(path.join(__dirname)))
-app.use('/dist',serveStatic(__dirname + "/dist"));
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
+app.get('/dong', function (req, res) {
+  res.send('hahaha')
 })
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + 'index.html'))
+})
 
 
-// "devv": "cross-env NODE_ENV=development webpack-dev-server --open --hot",
-// "buildd": "cross-env NODE_ENV=production webpack --progress --hide-modules",
-// "dev": "webpack-dev-server --inline --progress --config build/webpack.dev.conf.js",
-// "start": "node server.js",
-// "unit": "jest --config test/unit/jest.conf.js --coverage",
-// "e2e": "node test/e2e/runner.js",
-// "test": "npm run unit && npm run e2e",
-// "build": "node build/build.js"
+app.listen(5000, function () {
+  console.log( 'Express serving on 5000!' )
+})
